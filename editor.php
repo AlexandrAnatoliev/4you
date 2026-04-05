9cc78b2c47b6d2aa3328e96e4ac32bf76b6febdb
AlexandrAnatoliev <per-1986@list.ru>
2026-04-05 13:43:25 +0300

    fix: add styles

--- editor.php
+++ editor.php
@@ -26,6 +26,8 @@ if (file_exists($filename)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/styles/main.css">
+
     <title>Редактор файла</title>
     <style>
         body {
